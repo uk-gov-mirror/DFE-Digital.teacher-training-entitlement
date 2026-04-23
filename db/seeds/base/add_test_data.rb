16d23fcb287613669789427f50e88805685c1559
Jerome Riga <jriga@zemis.co.uk>
2026-04-23 17:06:23 +0100

    Update app seed
    use custom data instead of test scenarios

--- db/seeds/base/add_test_data.rb
+++ db/seeds/base/add_test_data.rb
@@ -1,3 +1,11 @@
 LeadProvider.find_each do |lead_provider|
-  ValidTestDataGenerators::APITestScenariosSeeder.new(lead_provider: lead_provider).call
+  seeder = ValidTestDataGenerators::APITestScenariosSeeder.new(
+    lead_provider:,
+    cohort_year: Time.zone.now.year - 2,
+  )
+
+  seeder.custom_data(
+    nb_cohort: 6,
+    nb_app_per_state: 20,
+  )
 end
