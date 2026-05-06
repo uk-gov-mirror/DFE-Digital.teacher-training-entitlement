bb1d8f2dddf9bfca2d1a38a6e672cea0463873c3
Toby Privett <toby@snaplab.co.uk>
2026-05-06 14:40:52 +0100

    Add TeacherAuth::RefreshToken service

---
+++ app/services/teacher_auth/refresh_token.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module TeacherAuth
+  class RefreshToken
+    include HTTParty
+
+    base_uri ENV.fetch("TEACHER_AUTH_DOMAIN", nil)
+
+    def initialize(refresh_token)
+      @refresh_token = refresh_token
+    end
+
+    def call
+      response = self.class.post(
+        "/oauth2/token",
+        body: {
+          grant_type: "refresh_token",
+          refresh_token: @refresh_token,
+          client_id: ENV.fetch("TEACHER_AUTH_CLIENT_ID", nil),
+          client_secret: ENV.fetch("TEACHER_AUTH_CLIENT_SECRET", nil),
+        },
+      )
+
+      if response.success?
+        {
+          access_token: response.parsed_response["access_token"],
+          refresh_token: response.parsed_response["refresh_token"],
+        }
+      else
+        Rails.logger.error("TeacherAuth::RefreshToken failed: #{response.code} - #{response.body}")
+        nil
+      end
+    end
+  end
+end
---
+++ spec/services/teacher_auth/refresh_token_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TeacherAuth::RefreshToken do
+  subject { described_class.new(refresh_token).call }
+
+  let(:refresh_token) { "old-refresh-token" }
+  let(:new_access_token) { "new-access-token" }
+  let(:new_refresh_token) { "new-refresh-token" }
+
+  let(:success_response) do
+    {
+      "access_token" => new_access_token,
+      "refresh_token" => new_refresh_token,
+    }
+  end
+
+  describe "#call" do
+    context "when the request is successful" do
+      before do
+        stub_request(:post, %r{/oauth2/token})
+          .with(body: hash_including(grant_type: "refresh_token", refresh_token:))
+          .to_return(status: 200, body: success_response.to_json, headers: { "Content-Type" => "application/json" })
+      end
+
+      it "returns the new tokens" do
+        expect(subject).to eq(access_token: new_access_token, refresh_token: new_refresh_token)
+      end
+    end
+
+    context "when the request fails" do
+      before do
+        stub_request(:post, %r{/oauth2/token})
+          .to_return(status: 400, body: { error: "invalid_grant" }.to_json)
+      end
+
+      it "returns nil" do
+        expect(subject).to be_nil
+      end
+    end
+  end
+end
