9761064c8d1e6fc80f7f92b26ec416a28a914a8b
Toby Privett <toby@snaplab.co.uk>
2026-05-06 14:36:35 +0100

    Add offline_access scope to request refresh_token

--- lib/omniauth/strategies/teacher_auth.rb
+++ lib/omniauth/strategies/teacher_auth.rb
@@ -11,7 +11,8 @@ module Omniauth
       option :send_scope_to_token_endpoint, false
 
       # This is a space separated string, comma separated will fail
-      option :scope, %i[email openid profile teaching_record].join(" ")
+      # offline_access is required to get a refresh_token for users without a TRN
+      option :scope, %i[email openid profile teaching_record offline_access].join(" ")
     end
   end
 end
