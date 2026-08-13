300cbdabb584e8343dcad02ba006b9167c930aa6
Ben Keeping <benjaminkeeping@gmail.com>
2026-08-13 14:34:16 +0100

    use academic_year not start_year

--- app/controllers/admin/delivery_partnerships_controller.rb
+++ app/controllers/admin/delivery_partnerships_controller.rb
@@ -2,6 +2,6 @@ class Admin::DeliveryPartnershipsController < AdminController
   def edit
     @delivery_partner = DeliveryPartner.find(params[:delivery_partner_id])
     @lead_providers = LeadProvider.all.order(:name)
-    @course_cohorts = CourseCohort.includes(:course, :cohort).order("cohorts.registration_starts_at DESC", "courses.name ASC")
+    @course_cohorts = CourseCohort.includes(:course, :cohort).order(:academic_year)
   end
 end
